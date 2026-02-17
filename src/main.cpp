1def87cef22118082f1de01198a93c10e5203ad4
Verdecryl <adrian.plesca.2006@gmail.com>
2026-02-17 18:36:39 +0200

    Refactor main.cpp to improve structure and readability

--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,23 @@
 #include <d3d11.h>
 #include <string>
 
-// ImGui Headers
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_dx11.h"
 #include "imgui/imgui_impl_win32.h"
 
-// Your Custom Headers
 #include "memory.h"
 #include "cheat.h"
 
-// Link DirectX libraries
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "dwmapi.lib")
 
-// Global variables for ImGui/DirectX
 ID3D11Device* device = nullptr;
 ID3D11DeviceContext* device_context = nullptr;
 IDXGISwapChain* swap_chain = nullptr;
 ID3D11RenderTargetView* render_target_view = nullptr;
 
-// External handler for Win32 window messages
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-// Window Procedure
 LRESULT CALLBACK window_procedure(HWND window, UINT message, WPARAM w_param, LPARAM l_param) {
     if (ImGui_ImplWin32_WndProcHandler(window, message, w_param, l_param)) {
         return 0L;
@@ -40,17 +34,14 @@ LRESULT CALLBACK window_procedure(HWND window, UINT message, WPARAM w_param, LPA
 }
 
 INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
-    // 1. Initialize Memory
     Memory mem("ac_client.exe");
     uintptr_t client = 0;
 
-    // Wait for the game to open if it's not already
     while (client == 0) {
         client = mem.GetModuleAddress("ac_client.exe");
         Sleep(500);
     }
 
-    // 2. Define Window Class
     WNDCLASSEXW wc{};
     wc.cbSize = sizeof(WNDCLASSEXW);
     wc.style = CS_HREDRAW | CS_VREDRAW;
@@ -59,7 +50,6 @@ INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
     wc.lpszClassName = L"OverlayClass";
     RegisterClassExW(&wc);
 
-    // 3. Create the Transparent Window
     const HWND window = CreateWindowExW(
         WS_EX_TOPMOST | WS_EX_TRANSPARENT | WS_EX_LAYERED,
         wc.lpszClassName,
@@ -73,7 +63,6 @@ INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
     const MARGINS margins = { -1 };
     DwmExtendFrameIntoClientArea(window, &margins);
 
-    // 4. Initialize DirectX 11
     DXGI_SWAP_CHAIN_DESC sd{};
     sd.BufferDesc.RefreshRate.Numerator = 60U;
     sd.BufferDesc.RefreshRate.Denominator = 1U;
@@ -95,7 +84,6 @@ INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
         back_buffer->Release();
     }
 
-    // 5. Initialize ImGui
     ImGui::CreateContext();
     ImGui::StyleColorsDark();
     ImGui_ImplWin32_Init(window);
@@ -103,11 +91,9 @@ INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
 
     ShowWindow(window, cmd_show);
 
-    // Cheat States
     bool bESP = true;
     bool bMenuOpen = true;
 
-    // 6. Main Loop
     bool running = true;
     while (running) {
         MSG msg;
@@ -119,10 +105,9 @@ INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
 
         if (!running) break;
 
-        // Toggle menu with Insert key
         if (GetAsyncKeyState(VK_INSERT) & 1) {
             bMenuOpen = !bMenuOpen;
-            // Update window styles for click-through
+
             long style = GetWindowLong(window, GWL_EXSTYLE);
             if (bMenuOpen) {
                 SetWindowLong(window, GWL_EXSTYLE, style & ~WS_EX_TRANSPARENT);
@@ -132,12 +117,10 @@ INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
             }
         }
 
-        // Start ImGui Frame
         ImGui_ImplDX11_NewFrame();
         ImGui_ImplWin32_NewFrame();
         ImGui::NewFrame();
 
-        // --- Hack Logic Start ---
         if (bMenuOpen) {
             ImGui::Begin("Adrian's AC External", &bMenuOpen, ImGuiWindowFlags_AlwaysAutoResize);
             ImGui::Checkbox("ESP Boxes", &bESP);
@@ -145,7 +128,6 @@ INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
             ImGui::End();
         }
 
-        // ESP Implementation
         if (bESP) {
             float vMatrix[16];
             mem.Read(client + offsets::ViewMatrix, &vMatrix, sizeof(vMatrix));
@@ -189,7 +171,6 @@ INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
         }
         // --- Hack Logic End ---
 
-        // Render ImGui
         ImGui::Render();
         const float clear_color[4] = { 0.f, 0.f, 0.f, 0.f };
         device_context->OMSetRenderTargets(1U, &render_target_view, nullptr);
@@ -198,7 +179,6 @@ INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
         swap_chain->Present(1U, 0U);
     }
 
-    // Cleanup
     ImGui_ImplDX11_Shutdown();
     ImGui_ImplWin32_Shutdown();
     ImGui::DestroyContext();
@@ -209,4 +189,5 @@ INT APIENTRY WinMain(HINSTANCE instance, HINSTANCE, PSTR, INT cmd_show) {
     if (render_target_view) render_target_view->Release();
 
     return 0;
+
 }
